97187a81a7371e818452a947942ba102179ce9ad
HJ Hubeek <hjhubeek@gmail.com>
2020-03-10 18:52:22 +0100

    init

---
+++ src/app/wikipedia.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WikipediaService {
+
+  constructor(private http: HttpClient) {
+
+
+  }
+
+  public search(term: string): Observable<object> {
+    return this.http.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*',
+      }
+    });
+  }
+}
